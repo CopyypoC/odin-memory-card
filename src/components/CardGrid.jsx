6111a632d215a7d877a22fae31ac6e6455352afc
CopyypoC <william.wang650@gmail.com>
2025-05-19 18:10:48 -0700

    feat: add CardGrid styles

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { fetchCards } from "../api/fetchCards.js";
+import "../styles/CardGrid.css";
 
 export function CardGrid() {
   const [imgList, setImgsList] = useState([]);
   const cards = imgList.map((item) => {
     return (
       <div className="card-container" key={item.url}>
-        <img src={item.url} alt="Game card" width="300" height="300" />
-        <p className="artist-name">{"@" + item.artist}</p>
+        <img className="card-img" src={item.url} alt="Game card" />
+        <a href={item.sourceUrl} target="_blank">
+          <p className="artist-name">{"@" + item.artist}</p>
+        </a>
       </div>
     );
   });
@@ -21,6 +24,7 @@ export function CardGrid() {
             id: item.id,
             url: item.url,
             artist: item.artist_name,
+            sourceUrl: item.source_url,
           };
         });
 
